6735fb7333e1912d14f1519d44512f51a32ac347
Boonchu <bigchoo@gmail.com>
2015-02-24 04:41:34 -0800

    NIc Teaming starting

---
+++ daily_linux/Case6-2.cmd
@@ -0,0 +1,9 @@
+##### Part Two - Nic Teaming RHCE exam
+* list ethernet devices prior adding to Virtualbox
+* promote two Nics to Virtualbox
+```
+3: enp0s8: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP qlen 1000
+    link/ether 08:00:27:64:1f:17 brd ff:ff:ff:ff:ff:ff
+4: enp0s9: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP qlen 1000
+    link/ether 08:00:27:d8:50:ac brd ff:ff:ff:ff:ff:ff
+```
