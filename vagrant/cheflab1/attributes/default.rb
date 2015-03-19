bc74dee8d1bdc4a7443bde3934281c45e6dfe93a
boonchu <bigchoo@gmail.com>
2015-03-19 07:08:44 -0700

    updated git vagrant1 branch

--- vagrant/cheflab1/attributes/default.rb
+++ vagrant/cheflab1/attributes/default.rb
@@ -1,2 +1,5 @@
-node.default['java']['install_flavor'] = 'openjdk'  # this is the default, but let's be EXPLICIT!
+node.default['java']['install_flavor'] = 'openjdk'  
 node.default['java']['jdk_version'] = '7'
+
+# solution: http://serverfault.com/questions/390840/how-does-one-get-tomcat-to-bind-to-ipv4-address
+node.default['tomcat']['catalina_options'] = '-Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses'
--- vagrant/cheflab1/recipes/default.rb
+++ vagrant/cheflab1/recipes/default.rb
@@ -7,10 +7,23 @@
 include_recipe 'yum'
 include_recipe 'java'
 include_recipe 'tomcat'
+include_recipe 'simple_iptables'
 
 cookbook_file "/var/lib/tomcat6/webapps/punter.war" do
 	source	"punter.war"
 	mode 00744
 	owner 'root'
 	group 'root'
+end
+
+# Allow SSH
+simple_iptables_rule "ssh" do
+  rule "--proto tcp --dport 22"
+  jump "ACCEPT"
+end
+
+# Allow TOMCAT
+simple_iptables_rule "tomcat" do
+  rule [ "--proto tcp --dport 8080" ]
+  jump "ACCEPT"
 end
