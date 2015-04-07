6ed5b05533a67ac540c6c63cfb238cf0c7fa241b
boonchu <bigchoo@gmail.com>
2015-04-07 12:39:45 -0700

    updated to have firewall manifest

---
+++ devops/puppet/master/Redis/manifests/init.pp
@@ -0,0 +1,47 @@
+class my_redis(
+	$type = 'master',
+	$master = 'dummy',
+) {
+	
+	class { 'redis::install': 
+    		redis_version  => '2:2.8.4-2',
+    		redis_package  => true,
+	}
+
+	# Allow Redis
+  	firewall { '100 allow redis access':
+    		port   => '6379',
+    		proto  => tcp,
+    		action => accept,
+  	}
+
+	service { 'redis-server': 
+		ensure => stopped,
+		enable => false,
+	}
+
+	if $type == 'master' {
+		redis::server { 'master':
+      			redis_memory    => '1g',
+      			redis_ip        => '0.0.0.0',
+      			redis_port      => 6379,
+      			running         => true,
+      			enabled         => true,
+      			requirepass     => 'redis',
+  		}
+	}
+
+	if $type == 'slave' {
+		redis::server { 'slave':
+      			redis_memory    => '1g',
+      			redis_ip        => '0.0.0.0',
+      			redis_port      => 6379,
+      			running         => true,
+      			enabled         => true,
+      			requirepass     => 'redis',
+      			slaveof         => $master,
+      			masterauth      => 'redis',
+  		}	
+	}
+
+}
